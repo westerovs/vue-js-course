ecab1effc207d0fb24f86c67eb91554f06092c05
Александр <aleksandr.vesterovskiy@neuro-city.ru>
2020-08-14 13:23:29 +0300

    Работа с пользовательским вводом

--- js/index.js
+++ js/index.js
@@ -14,12 +14,54 @@ const app2 = new Vue({
 })
 
 
+// Условия и циклы
 const app3 = new Vue({
     el: '#app-3',
     data: {
-      seen: true
+        seen: true
+    }
+})
+
+const app4 = new Vue({
+    el: '#app-4',
+    data: {
+        value: [
+            {li: 'to Fucking your mom !'},
+            {li: 'to Fucking your !'},
+            {li: 'to Fucking all your girlfriend !'},
+            {li: 'to Fucking this world !'},
+        ]
+    }
+})
+
+// Работа с пользовательским вводом
+const app5 = new Vue({
+    el: '#app-5',
+    data: {
+      message: 'Привет, Vue.js!'
+    },
+    methods: {
+      reverseMessage: function () {
+        this.message = this.message.split('').reverse().join('')
+      }
     }
   })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
